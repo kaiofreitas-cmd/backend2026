950c16b40395994fa52e293f05f7da04172ba96a
kaiofreitas-cmd <kaiofreitas@uni9.edu.br>
2026-03-19 00:30:18 +0000

    aula 18/3/2026

---
+++ src/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pintoplays</title>
+</head>
+<body>
+    <h1> Form - 18/03/26</h1>
+<form method="post" action="recebe.php">
+login<input type="text" id="login" name="login"><br>
+senha<input type="password" id="senha" name="senha"><br>
+<Hr>
+<input type="submit" value="Entrar">
+
+</form>
+ </body>
+</html>
